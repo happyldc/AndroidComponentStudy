cb26864880bb081dec469b500b8a9758323da850
happyldc <958499976@qq.com>
2020-04-21 11:28:53 +0800

    添加AndroidComponent_Base子模块 并调用

--- app/src/main/java/com/happyldc/androidcomponentstudy/MainActivity.java
+++ app/src/main/java/com/happyldc/androidcomponentstudy/MainActivity.java
@@ -3,6 +3,10 @@ package com.happyldc.androidcomponentstudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.happyldc.base.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void performAdd(View view) {
+        Toast.makeText(this,  Utils.add(1,2)+"", Toast.LENGTH_SHORT).show();
     }
 }
